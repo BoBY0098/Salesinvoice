195f96a2261fdb2a5ac8e045cc4640ffa5e2dbe6
amirhosseinjavid <amirhosenjavid@aol.com>
2023-07-15 14:05:15 +0330

    Authorization added to Controllers

--- src/main/java/com/amirhossein/salesinvoice/controllers/InvoiceManageController.java
+++ src/main/java/com/amirhossein/salesinvoice/controllers/InvoiceManageController.java
@@ -5,6 +5,7 @@ import com.amirhossein.salesinvoice.models.invoice.InvoiceRes;
 import com.amirhossein.salesinvoice.services.service.InvoiceService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.Authorization;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,13 +21,13 @@ public class InvoiceManageController {
     }
 
     @GetMapping()
-    @ApiOperation(value = "Get All Invoices" , notes = "Get All Invoices" , response = InvoiceRes.class , responseContainer = "List")
+    @ApiOperation(value = "Get All Invoices" , notes = "Get All Invoices" , response = InvoiceRes.class , responseContainer = "List" , authorizations = {@Authorization(value = "jwtToken")})
     public ResponseEntity<?> getAllInvoices(){
         return ResponseEntity.ok(invoiceService.getAllInvoices());
     }
 
     @PostMapping()
-    @ApiOperation(value = "Create New a Invoice" , notes = "Create New a Invoice" , response = InvoiceRes.class)
+    @ApiOperation(value = "Create New a Invoice" , notes = "Create New a Invoice" , response = InvoiceRes.class , authorizations = {@Authorization(value = "jwtToken")})
     public ResponseEntity<?> createInvoice(@RequestBody InvoiceReq invoiceReq){
         return ResponseEntity.ok(invoiceService.createInvoice(invoiceReq));
     }
--- src/main/java/com/amirhossein/salesinvoice/controllers/UserManageController.java
+++ src/main/java/com/amirhossein/salesinvoice/controllers/UserManageController.java
@@ -5,6 +5,7 @@ import com.amirhossein.salesinvoice.models.user.UserRes;
 import com.amirhossein.salesinvoice.services.service.UserService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.Authorization;
 import lombok.NonNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -31,19 +32,19 @@ public class UserManageController {
     }
 
     @GetMapping()
-    @ApiOperation(value = "Get All Users" , notes = "Get All Users" , response = UserRes.class , responseContainer = "List")
+    @ApiOperation(value = "Get All Users" , notes = "Get All Users" , response = UserRes.class , responseContainer = "List" , authorizations = {@Authorization(value = "jwtToken")})
     public List<UserRes> getAllUsers(){
         return userService.getAllUsers();
     }
 
     @GetMapping("/{userId}")
-    @ApiOperation(value = "Get a User" , notes = "Get a User by UserID" , response = UserRes.class)
+    @ApiOperation(value = "Get a User" , notes = "Get a User by UserID" , response = UserRes.class , authorizations = {@Authorization(value = "jwtToken")})
     public UserRes getUserById(@PathVariable UUID userId){
         return userService.getUserById(userId);
     }
 
     @PostMapping("/register")
-    @ApiOperation(value = "Create a New User" , notes = "Create a New User" , response = UserRes.class)
+    @ApiOperation(value = "Create a New User" , notes = "Create a New User" , response = UserRes.class , authorizations = {@Authorization(value = "jwtToken")})
     public UserRes createUser(@RequestBody UserReq req){
         return userService.createUser(req);
     }
